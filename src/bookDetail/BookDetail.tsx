6d3391a746bda8fe3f892d8882e152fe08ea0cc2
eleven <sydryy@gmail.com>
2022-10-20 07:42:54 -0700

    complete expand-collapse function

--- src/bookDetail/BookDetail.tsx
+++ src/bookDetail/BookDetail.tsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const BookDetail: React.FC<{ book: any }> = ({ book }) => {
-	// const overLength = book.description.length > 300
+	const [overLength, setOverLength] = useState(false)
+	const [expanded, setExpanded] = useState(false)
+	const [desc, setDesc] = useState(book.description)
+
+	const objEmpty = (obj: Object): boolean => {
+		return obj
+			&& Object.keys(obj).length === 0
+			&& Object.getPrototypeOf(obj) === Object.prototype
+	}
+
+	useEffect(() => {
+		if (objEmpty(book)) return
+		setOverLength(book.description?.length > 300)
+	}, [book])
+
+	useEffect(() => {
+		if (objEmpty(book)) return
+		if (!overLength) return
+		if (expanded) {
+			setDesc(book.description)
+			return
+		}
+		setDesc(`${book.description?.substring(0, 300)}...`)
+	}, [book, expanded, overLength])
 
 	return (<div data-test="book-detail">
 		<h2 className="title">{book.name}</h2>
 		<div className="description-wrapper">
-			<p className="description">{book.description}</p>
+			<p className="description">{desc}</p>
+			{overLength ? (
+				<div className="expand-bar">
+					{!expanded && <button className="expand-btn" onClick={() => setExpanded(true)}>View More</button>}
+					{expanded && <button className="collapse-btn" onClick={() => setExpanded(false)}>Collapse</button>}
+				</div>
+			) : (<></>)}
 		</div>
 	</div>)
 }
--- tests/BookDetail.test.tsx
+++ tests/BookDetail.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { describe, it, expect } from 'vitest'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import BookDetail from '../src/bookDetail/BookDetail'
 
 describe('BookDetail', () => {
@@ -17,20 +17,29 @@ describe('BookDetail', () => {
 		expect(container.querySelector('.description').innerHTML).toEqual(props.book.description)
 	})
 
-	// it('expand-collapse should work', () => {
-	// 	const props = {
-	// 		book: {
-	// 			id: 1,
-	// 			name: 'Refactoring',
-	// 			description: `Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software again.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software again.`
-	// 		}
-	// 	}
-	// 	const { container } = render(<BookDetail {...props} />)
-	// 	expect(container.querySelector('.description').innerHTML).toEqual(props.book.description.substring(0, 300) + '...')
-	//
-	// 	const expand = container.querySelector('.expand-btn')
-	// 	expand.click()
-	//
-	// 	expect(container.querySelector('.description').innerHTML).toEqual(props.book.description)
-	// })
+	it('expand-collapse should work', () => {
+		const props = {
+			book: {
+				id: 1,
+				name: 'Refactoring',
+				description: `Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software again.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software.Martin Fowler\'s Refactoring defined core ideas and techniques that hundreds of thousands of developers have used to improve their software again.`
+			}
+		}
+		const { container } = render(<BookDetail {...props} />)
+		const description = container.querySelector('.description')
+		const book = props.book
+
+		expect(description.innerHTML).toEqual(`${book.description?.substring(0, 300)}...`)
+
+		const expand = container.querySelector('.expand-btn')
+		fireEvent(
+			expand,
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true
+			})
+		)
+
+		expect(description.innerHTML).toEqual(book.description)
+	})
 })
